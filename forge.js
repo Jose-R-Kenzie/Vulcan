32109742e262fc3a7bade396da89959ad67fc6b6
Jose-R-Kenzie <jose.rodriguez@mykenzie.snhu.edu>
2025-12-22 05:53:09 -0800

    Attempt 3 of trying to fix forge fx/ui

--- forge.js
+++ forge.js
@@ -1,6 +1,5 @@
-
-// Mouse reactive forge lighting
 document.addEventListener("mousemove", (e) => {
   const light = document.querySelector(".mouse-light");
+  if (!light) return;
   light.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 });
